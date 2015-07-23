bc38aa788ed090fbe1de52df951078b7d506489c
Amelia Carpenter <ameliacarpenter3@gmail.com>
2015-07-23 16:04:26 +0000

    fix merge problem

--- app-controller.rb
+++ app-controller.rb
@@ -15,10 +15,4 @@ class MyApp < Sinatra::Base
       erb :bad
   end
   
-  post '/results' do
-    @user_sentence=params[:sentence]
-    @translated_sentence=to_pig_latin(@user_sentence)
-    erb :results
-  end
-  
 end

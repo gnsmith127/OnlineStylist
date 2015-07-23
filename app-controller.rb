97c4d1a973c3b0bdd6a58d12cf773338589857c9
Amelia Carpenter <ameliacarpenter3@gmail.com>
2015-07-23 16:57:48 +0000

    add multiple pant feature

--- app-controller.rb
+++ app-controller.rb
@@ -10,7 +10,8 @@ class MyApp < Sinatra::Base
 
   post '/results' do 
     @shirt_choice=params[:shirt]
-    if good_style(@shirt_choice)==true
+    @pant_choice=params[:pants]
+    if good_style(@shirt_choice)==true and good_style(@pant_choice)==true
       erb :good 
     else 
       erb :bad
--- models/onlinestylist.rb
+++ models/onlinestylist.rb
@@ -1,8 +1,9 @@
 def good_style (choice)
   styles={
-  :shirt => ["Black T-Shirt", "White T-Shirt", "Gray T-Shirt"]
+  :shirt => ["Black T-Shirt", "White T-Shirt", "Gray T-Shirt"],
+  :pants => ["Khaki Pants", "White T-Shirt", "Gray T-Shirt"]
   }
-  if choice==styles[:shirt][0]
+  if choice==styles[:shirt][0] or choice==styles[:pants][0]
     return true
   else
     return false

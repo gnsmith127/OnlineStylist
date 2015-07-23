313200400ee1a4d230feb530b9bc435173f92e66
Amelia Carpenter <ameliacarpenter3@gmail.com>
2015-07-23 19:04:47 +0000

    update backend

--- models/onlinestylist.rb
+++ models/onlinestylist.rb
@@ -9,9 +9,9 @@ def good_style (shirt_choice, pant_choice)
       :color => ["red", "orange", "yellow", "green", "blue", "purple"], ["navy blue", "black", "gray", "white", "print/graphic/plaid", "denim"]
       }
   }
-  if shirt_choice==styles[:shirt][:color][6] and pant_choice==styles[:pants][:color][7]
-    
-    elsif pant_choice==styles[:pants][:color][6] and shirt_choice==styles[:shirt][:color][7]
+  if (shirt_choice==styles[:shirt][:color][1][0] && pant_choice==styles[:pants][:color][1][1]) or (shirt_choice==styles[:pants][:color][1][1] && pant_choice==styles[:shirt][:color][1][0])
+    return false
+  elsif (shirt_choice==styles[:shirt][:color][1][2] && pant_choice==styles[:pants][:color][0][1]) or (shirt_choice==styles[:pants][:color][0][1] && pant_choice==styles[:shirt][:color][1][2])
     return false
   else
     return false

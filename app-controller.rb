c71529ae45db92a8dd6fa6c294fb3052003e9523
Amelia Carpenter <ameliacarpenter3@gmail.com>
2015-07-23 15:55:13 +0000

    make mvp backend

--- app-controller.rb
+++ app-controller.rb
@@ -1,17 +1,11 @@
 require 'bundler'
 Bundler.require
-require_relative 'models/pig_latinize.rb'
+require_relative 'models/onlinestylist.rb'
 
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
-  end
-
-  post '/results' do
-    @user_sentence=params[:sentence]
-    @translated_sentence=to_pig_latin(@user_sentence)
-    erb :results
   end
   
 end
---
+++ models/onlinestylist.rb
@@ -0,0 +1,11 @@
+styles={
+  :shirt => "tshirt"; "buttondown"
+  }
+
+def good_style (params)
+  if params==styles[0]
+    return true
+  else
+    return false
+  end
+end

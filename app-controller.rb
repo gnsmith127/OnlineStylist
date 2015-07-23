2f5f0026c9151756baae51dc4f3e3400b5851045
ssingh18 <saachi2020@gmail.com>
2015-07-23 16:03:17 +0000

    edit app-controller

--- app-controller.rb
+++ app-controller.rb
@@ -1,10 +1,18 @@
 require 'bundler'
 Bundler.require
+require_relative 'models/onlinestylist.rb'
 
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+
+  post '/results' do 
+    if good_style(params[:shirt])
+      erb :good 
+    else 
+      erb :bad
   end
   
 end
